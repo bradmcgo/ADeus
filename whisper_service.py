4c1cc45895c9b1150e755f996630df033f6d0275
Brad <bradme110@icloud.com>
2024-03-29 20:01:30 -0400

    added WhisperHallu support to minimize repetitive hallucinations.

--- whisper_service.py
+++ whisper_service.py
@@ -3,6 +3,9 @@ import whisper
 from werkzeug.utils import secure_filename
 import os
 from datetime import datetime
+from pydub import AudioSegment
+from transcribeHallu import loadModel
+from transcribeHallu import transcribePrompt
 
 app = Flask(__name__)
 
@@ -15,9 +18,14 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+def boost_volume(file_path, decibels=10):
+    audio_segment = AudioSegment.from_file(file_path)
+    louder_segment = audio_segment + decibels  # Increase volume by `decibels` dB
+    louder_segment.export(file_path, format="wav")
+
 def transcribe_audio(file_path):
     model = whisper.load_model("base")
-    result = model.transcribe(file_path)
+    result = model.transcribe(file_path, language="en", compression_ratio_threshold=1.35, temperature=0)
     return result['text']
 
 @app.route('/transcribe', methods=['POST'])
@@ -35,6 +43,35 @@ def transcribe():
         save_path = os.path.join(app.config['UPLOAD_FOLDER'], filename_with_timestamp)
         file.save(save_path)
         transcription = transcribe_audio(save_path)
+
+        # Boost the volume before transcribing
+        boost_volume(save_path, decibels=50)  # Adjust the decibels as necessary
+
+        ##### The audio language may be different from the one for the output transcription.
+        path = save_path
+        lngInput = "en"
+
+        # ##### Activate this for music file to get a minimal processing
+        isMusic = False
+
+        # ##### Need to be adapted for each language.
+        # ##### For prompt examples, see transcribeHallu.py getPrompt(lng:str)
+        lng = "en"
+        prompt = "Whisper, Ok. "\
+            +"A pertinent sentence for your purpose in your language. "\
+            +"Ok, Whisper. Whisper, Ok. "\
+            +"Ok, Whisper. Whisper, Ok. "\
+            +"Please find here, an unlikely ordinary sentence. "\
+            +"This is to avoid a repetition to be deleted. "\
+            +"Ok, Whisper. "
+
+        # ##### Model size to use
+        modelSize = "medium"
+        loadModel("0",modelSize=modelSize)
+
+        result = transcribePrompt(path=path, lng=lng, prompt=prompt, lngInput=lngInput,isMusic=isMusic)
+
         # Optionally, remove the file after processing
         os.remove(save_path)
-        return jsonify(transcription=transcription)
+        return jsonify(transcription=result)
+        
